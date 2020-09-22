2c41b28bde6558fadeff39358383eed3d2142a33
reconectar <arthurgbranco2@gmail.com>
2020-09-21 23:45:16 -0300

    removed comments and debugger line

--- weather-app/app.js
+++ weather-app/app.js
@@ -8,7 +8,9 @@ const location = 'Belo Horizonte'
 const mapboxApiKey = 'pk.eyJ1IjoiYXJ0aHVyYnJhbmNvIiwiYSI6ImNrZjhsMTJvYjBkNW8ycm8wZnZsMHgwdWsifQ.4xyUIszpTcH9B_ATRl-wDA'
 const mapboxApiUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURI(location)}.json?access_token=${mapboxApiKey}`
 
-debugger
+
+// Geocoding
+// Adress user input -> convert to Lat/Long -> input results on weather api url
 
 request({ url: mapboxApiUrl, json: true }, (error, response) => {
     if (error) {
@@ -24,17 +26,16 @@ request({ url: mapboxApiUrl, json: true }, (error, response) => {
 const weatherstackApiKey = '2e39b0ef029f5a044679549787ac79f3'
 const weatherstackApiUrl = `http://api.weatherstack.com/current?access_key=${weatherstackApiKey}&query=${encodeURI(location)}`
 
-// request({ url: weatherstackApiUrl, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to weather api!');
-//     } else if (response.body.error) {
-//         console.log('Unable to find location!')
-//     } else {
-//         const current = response.body.current
-//         console.log(`${current.weather_descriptions[0]}. ${current.temperature} degress out \
-// and feels like ${current.feelslike} degress.`)
-//     }
-// })
+// Weather
 
-// Geocoding
-// Adress user input -> convert to Lat/Long -> input results on weather api url
+request({ url: weatherstackApiUrl, json: true }, (error, response) => {
+    if (error) {
+        console.log('Unable to connect to weather api!');
+    } else if (response.body.error) {
+        console.log('Unable to find location!')
+    } else {
+        const current = response.body.current
+        console.log(`${current.weather_descriptions[0]}. ${current.temperature} degress out \
+and feels like ${current.feelslike} degress.`)
+    }
+})
