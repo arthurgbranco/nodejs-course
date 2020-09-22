5e97d69d94d9b62fe72147a3dc0b50f6b3593750
reconectar <arthurgbranco2@gmail.com>
2020-09-21 23:42:24 -0300

    added error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,26 +2,39 @@
 // https://account.mapbox.com/
 
 const request = require('request')
+
 const location = 'Belo Horizonte'
 
 const mapboxApiKey = 'pk.eyJ1IjoiYXJ0aHVyYnJhbmNvIiwiYSI6ImNrZjhsMTJvYjBkNW8ycm8wZnZsMHgwdWsifQ.4xyUIszpTcH9B_ATRl-wDA'
 const mapboxApiUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURI(location)}.json?access_token=${mapboxApiKey}`
 
-const coordinates = request({ url: mapboxApiUrl, json: true }, (error, response) => {
-    const results = response.body.features[0].center
-    return results.toString()
-})
+debugger
 
-console.log('coordinates: ' + coordinates);
+request({ url: mapboxApiUrl, json: true }, (error, response) => {
+    if (error) {
+        console.log('Unable to connect to geolocation api!');
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to find location!')
+    } else {
+        const results = response.body.features[0].center
+        console.log(results.toString())
+    }
+})
 
 const weatherstackApiKey = '2e39b0ef029f5a044679549787ac79f3'
-const weatherstackApiUrl = `http://api.weatherstack.com/current?access_key=${weatherstackApiKey}&query=${coordinates}&units=m`
+const weatherstackApiUrl = `http://api.weatherstack.com/current?access_key=${weatherstackApiKey}&query=${encodeURI(location)}`
 
-request({ url: weatherstackApiUrl, json: true }, (error, response) => {
-    const current = response.body.current
-    console.log(`${current.weather_descriptions[0]}. ${current.temperature} degress out \
-and feels like ${current.feelslike} degress.`)
-})
+// request({ url: weatherstackApiUrl, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather api!');
+//     } else if (response.body.error) {
+//         console.log('Unable to find location!')
+//     } else {
+//         const current = response.body.current
+//         console.log(`${current.weather_descriptions[0]}. ${current.temperature} degress out \
+// and feels like ${current.feelslike} degress.`)
+//     }
+// })
 
 // Geocoding
 // Adress user input -> convert to Lat/Long -> input results on weather api url
